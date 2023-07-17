c195350215f8e0f68684a5f18443eca64a25f6f5
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2023-07-17 12:22:41 +0545

    new tab optimized

--- src/functions.rs
+++ src/functions.rs
@@ -2,8 +2,9 @@ use crate::{app::App, tab::Tab};
 
 pub fn new_tab(_input: &str, _close: &mut bool, app: &mut App) {
     let (width, height) = app.get_editor_size();
-
-    app.tabs.push(Tab::new(width, height));
+    app.tabs
+        .insert(app.active_index + 1, Tab::new(width, height));
+    app.active_index += 1;
 }
 
 pub fn next_tab(_input: &str, _close: &mut bool, app: &mut App) {
